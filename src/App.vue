e7bf7f027e00f84f131127830eaf2c6c4665392b
elfi <alrigaff@gmail.com>
2021-03-11 20:28:07 +0300

    added search-input

--- src/App.vue
+++ src/App.vue
@@ -4,55 +4,87 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>{{ title }}</h1>
+          <!--message-->
           <message v-if="message" :message="message"/>
 
-          <!--        new note-->
-          <div class="new-note">
-            <input type="text"
-                   v-model="note.title">
-            <textarea v-model="note.desc"></textarea>
-            <button @click="addNode">
-              New note
-            </button>
-          </div>
-          <!--        note list-->
-          <div class="notes">
-            <div class="note"
-                 v-for="(note,index) in notes"
-                 :key="index">
-              <div class="note-header">
-                <p>{{ note.title }}</p>
-              </div>
-              <div class="note-body">
-                <p>{{ note.desc }}</p>
-                <span>{{ note.date }}</span>
-              </div>
+          <!--new note-->
+          <newNote
+              :note="note"
+              @addNote="addNote"/>
+
+
+          <div class="note-header" style="margin: 36px 0">
+            <!-- title-->
+            <h1>{{ title }}</h1>
+
+            <!--search-->
+            <search
+                :value="search"
+                placeholder="Find your note"
+                @search="search = $event"/>
+
+            <!--icons control-->
+            <div class="icons">
+              <svg
+                  :class="{ active: grid }"
+                  @click="grid = true"
+                  style="cursor: pointer"
+                  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <rect x="3" y="3" width="7" height="7"></rect>
+                <rect x="14" y="3" width="7" height="7"></rect>
+                <rect x="14" y="14" width="7" height="7"></rect>
+                <rect x="3" y="14" width="7" height="7"></rect>
+              </svg>
+              <svg
+                  :class="{ active: !grid }"
+                  @click="grid = false"
+                  style="cursor: pointer"
+                  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <line x1="8" y1="6" x2="21" y2="6"></line>
+                <line x1="8" y1="12" x2="21" y2="12"></line>
+                <line x1="8" y1="18" x2="21" y2="18"></line>
+                <line x1="3" y1="6" x2="3" y2="6"></line>
+                <line x1="3" y1="12" x2="3" y2="12"></line>
+                <line x1="3" y1="18" x2="3" y2="18"></line>
+              </svg>
             </div>
           </div>
+
+          <!-- note list-->
+          <notes
+              :notes="notesFilter"
+              :grid="grid"
+              @remove="removeNote"/>
 
         </div>
       </section>
     </div>
-    <!--    <div id="app">-->
-    <!--      <h2>hello</h2>-->
-    <!--      <img alt="Vue logo" src="./assets/logo.png">-->
-    <!--    </div>-->
+
 
   </div>
 </template>
 
 <script>
 import message from "@/components/Message.vue";
+import notes from "@/components/Notes.vue";
+import newNote from "@/components/NewNote.vue";
+import search from "@/components/Search.vue";
 
 export default {
   components: {
-    message
+    message,
+    notes,
+    newNote,
+    search,
   },
-  data () {
+  data() {
     return {
       title: 'Notes App',
+      search: '',
       message: null,
+      grid: true,
       note: {
         title: '',
         desc: '',
@@ -76,8 +108,25 @@ export default {
       ]
     }
   },
+  computed: {
+    notesFilter () {
+      let array = this.notes,
+          search = this.search
+      if (!search) return array
+      //Small
+      search = search.trim().toLowerCase()
+      //Filter
+      array = array.filter(function (item) {
+        if (item.title.toLowerCase().indexOf(search) !== -1) {
+          return item
+        }
+      })
+      // Error
+      return array
+    }
+  },
   methods: {
-    addNode() {
+    addNote() {
       let {title, desc} = this.note;
 
       if (title === "") {
@@ -92,6 +141,9 @@ export default {
       this.message = null;
       this.note.title = '';
       this.note.desc = '';
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1)
     }
   }
 }
